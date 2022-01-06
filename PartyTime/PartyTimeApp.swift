38d9d1a8769afe60a9721fc22dfc7dff99075fa2
Kevin Finn <superkevin@gmail.com>
2022-01-05 22:51:53 -0500

    Initial Commit

---
+++ PartyTime/PartyTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyTimeApp.swift
+//  PartyTime
+//
+//  Created by Kevin Finn on 1/5/22.
+//
+
+import SwiftUI
+
+@main
+struct PartyTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
